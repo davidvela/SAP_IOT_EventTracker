613216bc41537e0b5bef84e9659161c18c0a227e
VELA TIRADO DAVID <david.vela.tirado@gmail.com>
2017-04-11 23:29:36 +0000

    Add google maps to SIT application! testing purposes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,6 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
+	var map;
 
 	return Controller.extend("sit.controller.View1", {
 		onInit: function() {
@@ -31,8 +32,36 @@ sap.ui.define([
 				var element = oview.byId("__labelDistanceValue");
 				element.setText(distance.range_cm);
 			}
-			var timer = setInterval(startCountDown, interval);
+			//var timer = setInterval(startCountDown, interval);
 
+		},
+			onAfterRendering: function() {		
+			document.getElementById('idMaps1--map_canvas').setAttribute("style","height:80vh; width:80vh");				
+			//this.byId('map_canvas').setAttribute("style","height:100vh");				
+			google.maps.event.addDomListener(window, 'load', this.initMap());
+			this.findLocation();
+
+		},
+		findLocation: function(){
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition(function(position){
+				var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+				map.setCenter(pos);
+				var infowindow = new google.maps.InfoWindow({
+			        map: map,
+			        position: pos,
+			        content: 'This is you Location\n'+pos 
+			      });
+					});
+			}
+		
+		},
+		initMap: function(){
+				var mapOptions = {
+					    			zoom: 6
+								};
+				map = new google.maps.Map(document.getElementById('idMaps1--map_canvas'),mapOptions);
+		
 		}
 
 	});
